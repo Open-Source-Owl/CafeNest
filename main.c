4c079df942621bc0c32a3c1788c5f742053774ed
Open-Source-Owl <maverick.abdullah.007@gmail.com>
2025-01-22 10:18:18 +0600

    main.c

--- main.c
+++ main.c
@@ -1,87 +1,114 @@
-#include<stdio.h>
+/*
+ * cafemain.c
+ * University Autometed cafeteria - Main File
+ * Author: Team ProgBuddy
+ *
+ * Description:
+ * This is the main file for the University Café Management System.
+ * It serves as the entry point of the application, providing a menu-driven
+ * interface for both admins and customers to interact with the system.
+ */
+
+#include <stdio.h>
 #include "cafe.c"
 
-int main(){
-    MenuCategory *menu = readMenu();
+/*
+ * Main function of the program.
+ * Provides options for admin login, customer interaction, and system exit.
+ * @return 0 on successful execution.
+ */
+int main() {
+    MenuCategory *menu = readMenu(); // Load the menu from the file
     MenuCategory *currentCategory = menu;
-    unsigned int choice;
-     unsigned login =0;
-     while(1){
-        if (login ==0){
-            printf("\n Welcome to Automated Cafeteria\n");
+    uint32_t choice; // Variable to store user input for menu options
+    uint32_t login = 0; // Login state (0: not logged in, 1: logged in as admin)
+
+    while (1) {
+        if (login == 0) {
+            // Main menu for guests
+            printf("\nWelcome to Automated Cafeteria\n");
             printf("1. Log in as Admin\n");
             printf("2. Log in as Customer\n");
             printf("3. Exit\n");
             printf("Enter your choice: ");
-            scanf("%d", &choice);
-             switch (choice) {
+            scanf("%u", &choice);
+
+            switch (choice) {
                 case 1:
+                    // Admin login
                     if (adminlog()) {
                         login = 1;
                         printf("Welcome to the system.\n");
                     } else {
                         login = 0;
-                        printf("Return to main system.\n");
+                        printf("Returning to main menu.\n");
                     }
                     break;
                 case 2:
-                        takeorder(menu);
+                    // Customer interaction
+                    takeorder(menu);
                     break;
                 case 3:
+                    // Exit the program
                     printf("Exiting the program. Goodbye!\n");
                     exit(0);
                 default:
                     printf("Invalid choice. Please try again.\n");
-             }
-        }
-        else {
+            }
+        } else {
+            // Admin menu
             printf("\nAdmin Options:\n");
-
             printf("1. Update Menu\n");
-            printf("2. Display menu \n");
+            printf("2. Display Menu\n");
             printf("3. View Sales Report\n");
             printf("4. Clear Sales Report\n");
-            printf("5. Change password\n");
-            printf("6. Log out\n");
+            printf("5. Change Password\n");
+            printf("6. Log Out\n");
             printf("7. Exit\n");
-            unsigned int x=1;
+            uint32_t x = 1;
             printf("Enter your choice: ");
-            scanf("%d", &choice);
-            
-            switch (choice) {
+            scanf("%u", &choice);
 
+            switch (choice) {
                 case 1:
+                    // Update menu
                     updateMenu(menu);
                     break;
                 case 2:
-                     x=1;
-                     while(currentCategory){
-                     displayMenu(currentCategory,&x);
-                      currentCategory=currentCategory->next;
-                     }
-                     currentCategory=menu;
+                    // Display menu
+                    x = 1;
+                    while (currentCategory) {
+                        displayMenu(currentCategory, &x);
+                        currentCategory = currentCategory->next;
+                    }
+                    currentCategory = menu;
                     break;
                 case 3:
-                   viewsale();
+                    // View sales report
+                    viewsale();
                     break;
                 case 4:
+                    // Clear sales report
                     clearsalesreport();
                     break;
                 case 5:
-                     changePass();
+                    // Change admin password
+                    changePass();
                     break;
                 case 6:
-                   login = 0;
+                    // Log out
+                    login = 0;
                     printf("Admin logged out.\n");
                     break;
                 case 7:
+                    // Exit the program
                     printf("Exiting the program. Goodbye!\n");
                     exit(1);
-                    break;
                 default:
                     printf("Invalid choice. Please try again.\n");
             }
         }
-     }
- return 0;
+    }
+
+    return 0;
 }
